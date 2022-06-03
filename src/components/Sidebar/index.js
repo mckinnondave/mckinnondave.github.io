b42155882bcea8bf47dc6b259ed67797cd6d178f
Dave <dave.w.mckinnon@gmail.com>
2022-06-03 16:48:39 -0700

    add home page and animation components

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,7 +4,7 @@ import LogoS from '../../assets/images/logo-d.png';
 import LogoSubtitle from '../../assets/images/dlogo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons';
-import {faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import {faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -37,6 +37,11 @@ const Sidebar = () => (
       <li>
         <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/dave-mckinnon/">
           <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://github.com/mckinnondave">
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
         </a>
       </li>
     </ul>
