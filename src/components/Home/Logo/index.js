11dc29bca77af314ee7bd8c988ab04232a260a2b
Dave <dave.w.mckinnon@gmail.com>
2022-06-06 11:49:01 -0700

    add logo to homepage

---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,13 @@
+import './index.scss';
+import LogoD from '../../../assets/images/logo-d.png'
+
+
+const Logo = () => {
+  return (
+    <div className='logo-container'>
+      <img className='solid-logo' src={LogoD}/>
+    </div>
+  )
+}
+
+export default Logo
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import LogoTitle from '../../assets/images/logo-d.png'
 import { Link } from 'react-router-dom'
 import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
 import "./index.scss"
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState("text-animate")
   const nameArray = ['a', 'v', 'e', ' ', 'M', 'c', 'k', 'i', 'n', 'n', 'o', 'n']
   const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+     }, 4000);
+    return () => clearTimeout(timeout);
+   }, []);
 
   return (
     <div className="container home-page">
@@ -16,24 +24,23 @@ const Home = () => {
           <span className={letterClass}>H</span>
           <span className={`${letterClass} _12`}>i,</span>
           <br />
-          <span className={`${letterClass} _13`}>I,</span>
-          <span className={`${letterClass} _14`}>'m,</span>
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
         <img src={LogoTitle} alt="developer" />
         <AnimatedLetters letterClass={letterClass} 
         strArray={nameArray}
         idx={15}
         />
+        <br />
         <AnimatedLetters letterClass={letterClass} 
         strArray={jobArray}
-        idx={22}
+        idx={26}
         />
-        <br />
-        web developer
         </h1>
         <h2>Full Stack Developer</h2>
         <Link to='/contact' className="flat-button">CONTACT ME</Link>
       </div>
-
+      <Logo />
     </div>
   )
 }
