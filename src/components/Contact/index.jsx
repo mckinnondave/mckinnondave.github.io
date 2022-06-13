c26a4ca449d33f6e68046545491e7c1156cd84b1
Dave <dave.w.mckinnon@gmail.com>
2022-06-13 16:35:15 -0700

    add styling to contact form

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -28,6 +28,27 @@ const Contact = () => {
             I am interested in a Junior Developer role with a company that I can
             further develop my skills with and provide value for.
           </p>
+          <div className='contact-form'>
+            <form>
+              <ul>
+                <li className='half'>
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className='half'>
+                  <input type="email" name="email" placeholder="Email" required />
+                </li>
+                <li>
+                  <input placeholder="Subject" type="text" name="subject" required/>
+                </li>
+                <li>
+                  <textarea placeholder='Message' name='message' required></textarea>
+                </li>
+                <li>
+                  <input type="submit" className='flat-button' value="SEND"/>
+                </li>
+              </ul>
+            </form>
+          </div>
         </div>
       </div>
       <Loader type="pacman" />
