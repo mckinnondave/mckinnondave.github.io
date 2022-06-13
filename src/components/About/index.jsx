d64523ac8a20658afcf05423454659d1b5353900
Dave <dave.w.mckinnon@gmail.com>
2022-06-13 14:11:00 -0700

    update About page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -22,12 +22,36 @@ const About = () => {
             idx={15}
           />
         </h1>
-        <p>TEXT</p>
-        <p>TEXT</p>
-        <p>TEXT</p>
+        <p>
+          I'm a full stack developer from Vancouver, BC with a 9 year background
+          in Aviation. Because of Covid, I was forced to take a step away from
+          aviation and realized that during this time away, I had an opportunity
+          to find new interests. I took this time to study a number of courses
+          through Thompson Rivers University, but ultimately found a passion for
+          coding while working through a number of free online courses during
+          the summer of 2021. Since then, I've done my own studying, enrolled
+          into Lighthouse Labs, and have just recently graduated from that
+          program. While I enjoyed flying, I found a passion for code, and now
+          want to see where this new, exciting path might lead me.
+        </p>
+        <p>
+          TECH STACK Languages: Javascript, Ruby, HTML, CSS Frameworks,
+          Libraries, and Environments: NodeJS, ReactJS, Ajax, Express, EJS,
+          JQuery, Bootstrap, ActiveRecord, React Native, Rails, Sass, Google
+          APIs Testing: Storybook, Cypress, RSpec, Jest, Mocha, Chai Systems,
+          CMS, and Databases: SQL, Git
+        </p>
+        <p>
+          Previous experience includes operating commercial domestic flights in
+          Canada with Westjet Encore, scheduled passenger flights and Medevac
+          operations with Perimeter Aviation. As well, I have a background in
+          retail, where I worked in a Point of Sales position for Choices
+          Markets during Covid, and in several departments for Canadian Tire and
+          Rona prior to entering the aviation industry.
+        </p>
       </div>
     </div>
   );
 };
 
-export default About
+export default About;
