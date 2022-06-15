f6958d8e093b3a424a2ecd7861b44e5a99e7eecc
Dave <dave.w.mckinnon@gmail.com>
2022-06-14 22:06:55 -0700

    adjust navbar

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -20,12 +20,13 @@ const Portfolio = () => {
           <h1>
             <AnimatedLetters
               letterClass={letterClass}
-              strArray={['M', 'y', ' ', 'R', 'e', 'c', 'e', 'n', 't', ' ', 'P', 'r', 'o', 'j', 'c', 't', 's']}
+              strArray={['M', 'y', ' ', 'R', 'e', 'c', 'e', 'n', 't', ' ', 'P', 'r', 'o', 'j', 'e', 'c', 't', 's']}
               idx={15}
             />
           </h1>
           <p>
-            <h3>HOPPER</h3>A react based application designed to simplify the
+            <a href="https://github.com/mckinnondave/Hopper">Hopper</a><br />
+            A react based application designed to simplify the
             decision making process when out with friends during a pub crawl.
             A user can select where they would like to
             begin their trip, and have pubs be randomly given to them that are
@@ -34,7 +35,7 @@ const Portfolio = () => {
             information to the user.
           </p>
           <p>
-            <h3>Graigslist</h3>
+          <a href="https://github.com/mckinnondave/Graigslist">Graigslist</a><br />
             Graigslist is an app that allows fictitious users to create items
             for sale, have those items saved to a database, and then displayed
             them on various pages. The items can be searched for and favourited
@@ -43,15 +44,15 @@ const Portfolio = () => {
             allow users and owners to interact with one another.
           </p>
           <p>
-            <h3>Scheduler</h3>
-            Interview Scheduler is a single page application (SPA) built using
+          <a href="https://github.com/mckinnondave/scheduler">Scheduler</a><br />
+            Scheduler is a single page application (SPA) built using
             React. Interviews can be booked during Monday and Friday, and a user
             can either edit/delete their bookings. The client application
             communicates with an API server over HTTP and users data is
             persisted by the API server using a PostgreSQL database.
           </p>
           <p>
-            <h3>Tweeter</h3>
+            <a href="https://github.com/mckinnondave/tweeter">Tweeter</a><br />
             Tweeter is a simple, single-page Twitter clone. This repository is
             the starter code for the project: Students will fork and clone this
             repository, then build upon it to practice their HTML, CSS, JS,
